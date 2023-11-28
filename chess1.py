3740236f36c917afb9a431f4df3d23349fa97ea6
disha8073 <142570265+disha8073@users.noreply.github.com>
2023-11-28 21:46:43 +0530

    Add files via upload

---
+++ chess1.py
@@ -0,0 +1,29 @@
+#write a python program to construct the  board
+import turtle
+p=turtle.Turtle()
+p.pensize(5)
+p.speed(9)
+
+def square():
+    for i in range(4):
+        p.forward(40)
+        p.right(90)
+    p.forward(40)
+
+for i in range(8):
+    p.penup()
+    p.goto(0,40*i) 
+    p.pendown()
+
+    for x in range(8):
+        if (x+i)%2 ==0:
+            color="black"
+        else:
+            color="white"
+
+        p.fillcolor(color)
+        p.begin_fill()
+        square()
+        p.end_fill()
+        
+    
